b1df3797412d11c55da882f2af862239f50e222a
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-06-06 00:01:33 +1000

    Sends the unix time based on NTP in protobuf
    We now send the current unix time in the encoded protobuf message. The
    NTP time is grabbed at setup (in the future this will be grabbed at regular
    intervals) and we compute the difference between the millis when we
    grabbed the time and the current millis, then add this to the NTP datum
    to get the current unix time.

--- src/LightPetClient/LightPetClient.ino
+++ src/LightPetClient/LightPetClient.ino
@@ -87,6 +87,9 @@ void sendDataPacketCallback();
 void sendClientServiceMessageCallback();
 void listenForUDPPacketCallback();
 
+// function prototype for ntpUnixTime
+unsigned long ntpUnixTime(WiFiUDP &udp);
+
 // Task objects used for task scheduling
 Task readMic(MS_PER_SECOND / MICROPHONE_SAMPLE_RATE, TASK_FOREVER, &readMicCallback);
 Task readLight(MS_PER_SECOND / LIGHT_SAMPLE_RATE, TASK_FOREVER, &readLightCallback);
@@ -106,6 +109,11 @@ IPAddress serverIP(0, 0, 0, 0);
 
 // A UDP instance
 WiFiUDP udp;
+
+// Create a variable to hold the Unix time during setup, as well as a variable to hold the current millis when
+// we got this time. This way we can compute current NTP time by comparison
+unsigned long NTPSyncTime;
+unsigned long NTPSyncMillis;
 
 // Set up the server magic string in a constant string variable
 const char serverMagicString[] = SERVER_SERVICE_MESSAGE;
@@ -152,6 +160,12 @@ void setup() {
   sendDataPacket.enable();
   sendClientServiceMessage.enable();
   listenForUDPPacket.enable();
+
+  // Get the time from an NTP server. For the moment we will just do this once, but it might make sense to do this
+  // as a task every so often to prevent clock drift
+  static WiFiUDP timeUDP;
+  NTPSyncTime = ntpUnixTime(timeUDP);
+  NTPSyncMillis = millis();
 }
 
 // Helper function that configures the input select lines of the MUX so that the mic is feeding data to A0
@@ -225,7 +239,10 @@ void sendDataPacketCallback() {
   // Zero our sensor data object
   outputData = SensorData_init_zero;
   // Set some fields in the output data
-  outputData.timestamp = millis();
+  // Compute the current NTP time with currentmillis - syncmillis + synctime
+  // FIXME: This will have issues if millis overflows back to 0. This should probably trigger an NTP sync. Set this
+  // up once we have a ntp sync task
+  outputData.timestamp = millis() - NTPSyncMillis + NTPSyncTime;
   outputData.has_timestamp = true;
 
   outputData.temperatureSampleRate = TEMP_HUMIDITY_SAMPLE_RATE;
@@ -424,6 +441,78 @@ bool encodeAudioData(pb_ostream_t *stream, const pb_field_t *field, void * const
   }
 
   return true;
+}
+
+/*
+ * © Francesco Potortì 2013 - GPLv3 - Revision: 1.13
+ *
+ * Send an NTP packet and wait for the response, return the Unix time
+ *
+ * To lower the memory footprint, no buffers are allocated for sending
+ * and receiving the NTP packets.  Four bytes of memory are allocated
+ * for transmision, the rest is random garbage collected from the data
+ * memory segment, and the received packet is read one byte at a time.
+ * The Unix time is returned, that is, seconds from 1970-01-01T00:00.
+ */
+
+unsigned long inline ntpUnixTime(WiFiUDP &udp)
+{
+  static int udpInited = udp.begin(123); // open socket on arbitrary port
+
+  const char timeServer[] = "pool.ntp.org";  // NTP server
+
+  // Only the first four bytes of an outgoing NTP packet need to be set
+  // appropriately, the rest can be whatever.
+  const long ntpFirstFourBytes = 0xEC0600E3; // NTP request header
+
+  // Fail if WiFiUdp.begin() could not init a socket
+  if (! udpInited)
+    return 0;
+
+  // Clear received data from possible stray received packets
+  udp.flush();
+
+  // Send an NTP request
+  if (! (udp.beginPacket(timeServer, 123) // 123 is the NTP port
+   && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
+   && udp.endPacket()))
+    return 0;       // sending request failed
+
+  // Wait for response; check every pollIntv ms up to maxPoll times
+  const int pollIntv = 150;   // poll every this many ms
+  const byte maxPoll = 15;    // poll up to this many times
+  int pktLen;       // received packet length
+  for (byte i=0; i<maxPoll; i++) {
+    if ((pktLen = udp.parsePacket()) == 48)
+      break;
+    delay(pollIntv);
+  }
+  if (pktLen != 48)
+    return 0;       // no correct packet received
+
+  // Read and discard the first useless bytes
+  // Set useless to 32 for speed; set to 40 for accuracy.
+  const byte useless = 40;
+  for (byte i = 0; i < useless; ++i)
+    udp.read();
+
+  // Read the integer part of sending time
+  unsigned long time = udp.read();  // NTP time
+  for (byte i = 1; i < 4; i++)
+    time = time << 8 | udp.read();
+
+  // Round to the nearest second if we want accuracy
+  // The fractionary part is the next byte divided by 256: if it is
+  // greater than 500ms we round to the next second; we also account
+  // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
+  // additionally, we account for how much we delayed reading the packet
+  // since its arrival, which we assume on average to be pollIntv/2.
+  time += (udp.read() > 115 - pollIntv/8);
+
+  // Discard the rest of the packet
+  udp.flush();
+
+  return time - 2208988800ul;   // convert NTP time to Unix time
 }
 
 void loop() {
