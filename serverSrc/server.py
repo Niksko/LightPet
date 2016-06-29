6141cdc0712c835d2888b65c399bb083e2a4be00
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-06-29 18:24:17 +1000

    Make code asynchronous using curio
    This makes the sending of server advertisements and the receiving of
    data asynchronous (I think).

--- serverSrc/server.py
+++ serverSrc/server.py
@@ -3,13 +3,31 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
 
+from curio.socket import *
+import curio
 from SensorData import SensorData
-from socket import *
 from LightPet.common.write_config_header import load_config_fromfile
 from google.protobuf.message import DecodeError
 
+# How often to advertise that we are a server
+SERVER_ADVERTISEMENT_TIMEOUT = 10
 
-def main():
+async def advertise(socket, message, port):
+    """Advertise the service on the given socket then wait"""
+    await socket.sendto(message.encode('ASCII'), ("10.0.0.255", port))
+    print("Sent server advertisement")
+    await curio.sleep(SERVER_ADVERTISEMENT_TIMEOUT)
+
+async def data_handler(data):
+        # Get that data into an object
+        data_object = SensorData()
+        try:
+            data_object.ParseFromString(data)
+            print(str(data_object))
+        except DecodeError:
+            print("Received non-protobuf encoded message (probably an advertisement)")
+
+async def main():
     # First, load the config from the config file
     config = load_config_fromfile('common/configuration.json')
 
@@ -23,20 +41,14 @@ def main():
     udpSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     udpSocket.bind(('', udp_port))
 
-    # Send a server advertisement
-    udpSocket.sendto(server_advertisement_message.encode('ASCII'), ("10.0.0.255", udp_port))
+    # Spawn the server advertisements
+    advertisement_task = await curio.spawn(advertise(udpSocket, server_advertisement_message, udp_port))
 
     # Receive messages and output them
     while True:
-        data, addr = udpSocket.recvfrom(512)
-
-        # Get that data into an object
-        data_object = SensorData()
-        try:
-            data_object.ParseFromString(data)
-            print(str(data_object))
-        except DecodeError:
-            print("Received non-protobuf encoded message (probably an advertisement)")
+        data, _ = await udpSocket.recvfrom(512)
+        # Spawn a data_handler task to handle this data
+        await curio.spawn(data_handler(data))
 
 if __name__ == '__main__':
-    main()
+    curio.run(main())
