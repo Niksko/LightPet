fdbe8ae9f908ecc0d3bf7e8922f165d14ef2b5bf
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-06-30 14:47:51 +1000

    Add logging of data and status
    This adds logging of information such as advertisements and logging of
    data to a file in json format.
    
    In order to separate the data from the information about advertisments,
    we set an attribute on the data LogRecords that indicates that they are
    data (record.data=True) and then filter these at the file handler that
    logs this data.

--- serverSrc/server.py
+++ serverSrc/server.py
@@ -5,6 +5,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from curio.socket import *
 import curio
+import logging
+import json
 from SensorData import SensorData
 from LightPet.common.write_config_header import load_config_fromfile
 from google.protobuf.message import DecodeError
@@ -14,21 +16,38 @@ SERVER_ADVERTISEMENT_TIMEOUT = 10
 
 async def advertise(socket, message, port):
     """Advertise the service on the given socket then wait"""
+    logger = logging.getLogger(__name__)
     while True:
         await socket.sendto(message.encode('ASCII'), ("10.0.0.255", port))
-        print("Sent server advertisement")
+        logger.info("Sent server advertisement")
         await curio.sleep(SERVER_ADVERTISEMENT_TIMEOUT)
 
 async def data_handler(data):
-        # Get that data into an object
-        data_object = SensorData()
-        try:
-            data_object.ParseFromString(data)
-            print(str(data_object))
-        except DecodeError:
-            print("Received non-protobuf encoded message (probably an advertisement)")
+    logger = logging.getLogger(__name__)
+    # Get that data into an object
+    data_object = SensorData()
+    try:
+        data_object.ParseFromString(data)
+        logger.info(json.dumps(data_object.asDict()), extra={'data': True})
+    except DecodeError:
+        logger.info("Received non-protobuf encoded message (probably an advertisement)")
+
+def filter_data(record):
+    # Filters records that don't have the data attribute set to true
+    try:
+        return record.data
+    except AttributeError:
+        return False
 
 async def main():
+    # Set up a logger for this module
+    logging.basicConfig(level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
+    fh = logging.FileHandler('log.txt')
+    fh.addFilter(filter_data)
+    logger.addHandler(fh)
+
     # First, load the config from the config file
     config = load_config_fromfile('common/configuration.json')
 
