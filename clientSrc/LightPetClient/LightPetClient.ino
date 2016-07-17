caa8968074cd71b25379d7a2aca45423f3ae9cb2
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-07-17 17:42:49 +1000

    Refactor to clean up code
    This moves the ntp time sync code to a separate file. We also move
    definition of configuration values to the configuration.h file. The
    autogeneration of these values from JSON will be added to the
    write_config_header file in a future commit

--- clientSrc/LightPetClient/LightPetClient.ino
+++ clientSrc/LightPetClient/LightPetClient.ino
@@ -22,35 +22,6 @@
 #define SDA_PIN D2
 #define ANALOG_PIN A0
 
-// Rates at which data is read from sensors. Values are in Hz
-#define MICROPHONE_SAMPLE_RATE 20000
-#define LIGHT_SAMPLE_RATE 20000
-#define TEMP_HUMIDITY_SAMPLE_RATE 1
-
-// Define how much to buffer our data arrays by, in case of timing errors causing them to fill before they're emptied
-#define DATA_BUFFER_SIZE 3
-
-// Just to avoid magic numbers, define the ms in a second
-#define MS_PER_SECOND 1000
-
-// Define how often to generate a data packet. Value in Hz
-#define DATA_SEND_RATE 50
-
-// Define how often to check for received UDP packets. Value in Hz
-#define DATA_RECEIVE_RATE 50
-
-// Define the size of the output buffer
-#define OUTPUT_BUFFER_SIZE 8092
-
-// Define how often we advertise ourselves in seconds
-#define ADVERTISEMENT_RATE 10
-
-// Define the max size of our udp packet
-#define UDP_MAX_SIZE 8092
-
-// Define how often to resync with the NTP server in ms
-#define NTP_SYNC_TIMEOUT 3600
-
 // Make a typdef to define an array with associated size. This will be used to encode packed arrays in protobufs
 typedef struct _ArrayWithSize {
   uint32_t **array;
@@ -364,78 +335,6 @@ bool encodePackedArray(pb_ostream_t *stream, const pb_field_t *field, void * con
   }
 
   return true;
-}
-
-/*
- * © Francesco Potortì 2013 - GPLv3 - Revision: 1.13
- *
- * Send an NTP packet and wait for the response, return the Unix time
- *
- * To lower the memory footprint, no buffers are allocated for sending
- * and receiving the NTP packets.  Four bytes of memory are allocated
- * for transmision, the rest is random garbage collected from the data
- * memory segment, and the received packet is read one byte at a time.
- * The Unix time is returned, that is, seconds from 1970-01-01T00:00.
- */
-
-uint64_t inline ntpUnixTime(WiFiUDP &udp)
-{
-  static int udpInited = udp.begin(123); // open socket on arbitrary port
-
-  const char timeServer[] = "pool.ntp.org";  // NTP server
-
-  // Only the first four bytes of an outgoing NTP packet need to be set
-  // appropriately, the rest can be whatever.
-  const long ntpFirstFourBytes = 0xEC0600E3; // NTP request header
-
-  // Fail if WiFiUdp.begin() could not init a socket
-  if (! udpInited)
-    return 0;
-
-  // Clear received data from possible stray received packets
-  udp.flush();
-
-  // Send an NTP request
-  if (! (udp.beginPacket(timeServer, 123) // 123 is the NTP port
-   && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
-   && udp.endPacket()))
-    return 0;       // sending request failed
-
-  // Wait for response; check every pollIntv ms up to maxPoll times
-  const int pollIntv = 150;   // poll every this many ms
-  const byte maxPoll = 15;    // poll up to this many times
-  int pktLen;       // received packet length
-  for (byte i=0; i<maxPoll; i++) {
-    if ((pktLen = udp.parsePacket()) == 48)
-      break;
-    delay(pollIntv);
-  }
-  if (pktLen != 48)
-    return 0;       // no correct packet received
-
-  // Read and discard the first useless bytes
-  // Set useless to 32 for speed; set to 40 for accuracy.
-  const byte useless = 40;
-  for (byte i = 0; i < useless; ++i)
-    udp.read();
-
-  // Read the integer part of sending time
-  unsigned long time = udp.read();  // NTP time
-  for (byte i = 1; i < 4; i++)
-    time = time << 8 | udp.read();
-
-  // Round to the nearest second if we want accuracy
-  // The fractionary part is the next byte divided by 256: if it is
-  // greater than 500ms we round to the next second; we also account
-  // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
-  // additionally, we account for how much we delayed reading the packet
-  // since its arrival, which we assume on average to be pollIntv/2.
-  time += (udp.read() > 115 - pollIntv/8);
-
-  // Discard the rest of the packet
-  udp.flush();
-
-  return (uint64_t) time - 2208988800ul;   // convert NTP time to Unix time
 }
 
 void loop() {
